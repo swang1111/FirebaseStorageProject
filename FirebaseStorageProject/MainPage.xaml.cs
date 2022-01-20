d9dd6c72742af17cfad86dbd12bf11ec3a942c42
shirleylidman <shirleylidman.sl@gmail.com>
2022-01-20 21:02:40 +0100

    Added simple view using tutorial
    Uncomment the code that's commented out when the project is complete.

--- FirebaseStorageProject/MainPage.xaml.cs
+++ FirebaseStorageProject/MainPage.xaml.cs
@@ -1,18 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using Xamarin.Forms;
+using System.Diagnostics;
+using System.IO;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 
 namespace FirebaseStorageProject
 {
     public partial class MainPage : ContentPage
     {
+        /*FirebaseStorageHelper firebaseStorageHelper = new FirebaseStorageHelper();
+        MediaFile file;*/
         public MainPage()
         {
             InitializeComponent();
         }
+
+        /*protected async override void OnAppearing()
+        {
+
+            base.OnAppearing();
+
+        }
+
+
+        private async void BtnPick_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+            try
+            {
+                file = await Plugin.Media.CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+                {
+                    PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium
+                });
+                if (file == null)
+                    return;
+                imgChoosed.Source = ImageSource.FromStream(() =>
+                {
+                    var imageStram = file.GetStream();
+                    return imageStram;
+                });
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        private async void BtnUpload_Clicked(object sender, EventArgs e)
+        {
+            await firebaseStorageHelper.UploadFile(file.GetStream(), Path.GetFileName(file.Path));
+        }
+        private async void BtnDelete_Clicked(object sender, EventArgs e)
+        {
+            await firebaseStorageHelper.DeleteFile(txtFileName.Text);
+            lblPath.Text = string.Empty;
+            await DisplayAlert("Success", "Deleted", "OK");
+        }
+
+        private async void BtnDownload_Clicked(object sender, EventArgs e)
+        {
+            string path = await firebaseStorageHelper.GetFile(txtFileName.Text);
+            if (path != null)
+            {
+                lblPath.Text = path;
+                await DisplayAlert("Success", path, "OK");
+            }
+
+        }*/
+
     }
 }
+
